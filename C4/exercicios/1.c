5b4675ad48b71b43c3bc0a70481a732c94f10096
debian <usr@buster>
2021-01-07 22:42:31 -0300

    C4

---
+++ C4/exercicios/1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void){
+  int c;
+  c = 0;
+  while(c++ <= 10)
+    printf("%d ",c);
+
+  printf("\n");
+
+  for(int i = 0;i<= 10;i++){
+     printf("%d ",i);
+  
+  
+  }
+ 
+  printf("\n");
+}
